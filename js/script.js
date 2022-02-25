b52bd804b3ce8d3b0bb9ee61c667e2fdb1f7a234
Alessio Caruso <caruso.alessio1999@gmail.com>
2022-02-25 17:23:54 +0100

    added dice images to the dice game

--- js/script.js
+++ js/script.js
@@ -23,11 +23,119 @@ document.getElementById("play").addEventListener('click', function () {
   console.log(diceBot);
   if (diceUser > diceBot){
     console.log("hai vinto");
+    document.getElementById("winner").innerHTML=("Hai Vinto!");
   }
   else if( diceUser==diceBot){
     console.log("abbiamo pareggiato");
+    document.getElementById("winner").innerHTML=("Abbiamo pareggiato!");
   }
   else{
     console.log("ha vinto il computer");
-  }
+    document.getElementById("winner").innerHTML=("Hai Perso!");
+    }
+    
+    // I add and remove the display none class to the image of the data assigned to the user
+    switch (true) {
+      case (diceUser==1):
+        document.getElementById("dice-1-user").classList.remove("d-none");
+        document.getElementById("dice-2-user").classList.add("d-none");
+        document.getElementById("dice-3-user").classList.add("d-none");
+        document.getElementById("dice-4-user").classList.add("d-none");
+        document.getElementById("dice-5-user").classList.add("d-none");
+        document.getElementById("dice-6-user").classList.add("d-none");
+        
+        break;
+      case (diceUser==2):
+        document.getElementById("dice-2-user").classList.remove("d-none");
+        document.getElementById("dice-1-user").classList.add("d-none");
+        document.getElementById("dice-3-user").classList.add("d-none");
+        document.getElementById("dice-4-user").classList.add("d-none");
+        document.getElementById("dice-5-user").classList.add("d-none");
+        document.getElementById("dice-6-user").classList.add("d-none");
+        break;
+      case (diceUser==3):
+        document.getElementById("dice-3-user").classList.remove("d-none");
+        document.getElementById("dice-1-user").classList.add("d-none");
+        document.getElementById("dice-2-user").classList.add("d-none");
+        document.getElementById("dice-4-user").classList.add("d-none");
+        document.getElementById("dice-5-user").classList.add("d-none");
+        document.getElementById("dice-6-user").classList.add("d-none");
+        break;
+      case (diceUser==4):
+        document.getElementById("dice-4-user").classList.remove("d-none");
+        document.getElementById("dice-1-user").classList.add("d-none");
+        document.getElementById("dice-2-user").classList.add("d-none");
+        document.getElementById("dice-3-user").classList.add("d-none");
+        document.getElementById("dice-5-user").classList.add("d-none");
+        document.getElementById("dice-6-user").classList.add("d-none");
+        break;
+      case (diceUser==5):
+        document.getElementById("dice-5-user").classList.remove("d-none");
+        document.getElementById("dice-1-user").classList.add("d-none");
+        document.getElementById("dice-2-user").classList.add("d-none");
+        document.getElementById("dice-3-user").classList.add("d-none");
+        document.getElementById("dice-4-user").classList.add("d-none");
+        document.getElementById("dice-6-user").classList.add("d-none");
+        break;
+      case (diceUser==6):
+        document.getElementById("dice-6-user").classList.remove("d-none");
+        document.getElementById("dice-1-user").classList.add("d-none");
+        document.getElementById("dice-2-user").classList.add("d-none");
+        document.getElementById("dice-3-user").classList.add("d-none");
+        document.getElementById("dice-4-user").classList.add("d-none");
+        document.getElementById("dice-5-user").classList.add("d-none");
+        break;
+    }
+    // I add and remove the display none class to the image of the data assigned to the bot
+    switch (true) {
+      case (diceBot==1):
+        document.getElementById("dice-1-bot").classList.remove("d-none");
+        document.getElementById("dice-2-bot").classList.add("d-none");
+        document.getElementById("dice-3-bot").classList.add("d-none");
+        document.getElementById("dice-4-bot").classList.add("d-none");
+        document.getElementById("dice-5-bot").classList.add("d-none");
+        document.getElementById("dice-6-bot").classList.add("d-none");
+        
+        break;
+      case (diceBot==2):
+        document.getElementById("dice-2-bot").classList.remove("d-none");
+        document.getElementById("dice-1-bot").classList.add("d-none");
+        document.getElementById("dice-3-bot").classList.add("d-none");
+        document.getElementById("dice-4-bot").classList.add("d-none");
+        document.getElementById("dice-5-bot").classList.add("d-none");
+        document.getElementById("dice-6-bot").classList.add("d-none");
+        break;
+      case (diceBot==3):
+        document.getElementById("dice-3-bot").classList.remove("d-none");
+        document.getElementById("dice-1-bot").classList.add("d-none");
+        document.getElementById("dice-2-bot").classList.add("d-none");
+        document.getElementById("dice-4-bot").classList.add("d-none");
+        document.getElementById("dice-5-bot").classList.add("d-none");
+        document.getElementById("dice-6-bot").classList.add("d-none");
+        break;
+      case (diceBot==4):
+        document.getElementById("dice-4-bot").classList.remove("d-none");
+        document.getElementById("dice-1-bot").classList.add("d-none");
+        document.getElementById("dice-2-bot").classList.add("d-none");
+        document.getElementById("dice-3-bot").classList.add("d-none");
+        document.getElementById("dice-5-bot").classList.add("d-none");
+        document.getElementById("dice-6-bot").classList.add("d-none");
+        break;
+      case (diceBot==5):
+        document.getElementById("dice-5-bot").classList.remove("d-none");
+        document.getElementById("dice-1-bot").classList.add("d-none");
+        document.getElementById("dice-2-bot").classList.add("d-none");
+        document.getElementById("dice-3-bot").classList.add("d-none");
+        document.getElementById("dice-4-bot").classList.add("d-none");
+        document.getElementById("dice-6-bot").classList.add("d-none");
+        break;
+      case (diceBot==6):
+        document.getElementById("dice-6-bot").classList.remove("d-none");
+        document.getElementById("dice-1-bot").classList.add("d-none");
+        document.getElementById("dice-2-bot").classList.add("d-none");
+        document.getElementById("dice-3-bot").classList.add("d-none");
+        document.getElementById("dice-4-bot").classList.add("d-none");
+        document.getElementById("dice-5-bot").classList.add("d-none");
+        break;
+    }
 })
